94c8f7c0fd19170530fb9f16946c75926e5639a4
yakobino1989 <yakobino1989@gmail.com>
2018-11-06 10:40:00 +0200

    add proj5

--- proj5/Program.cs
+++ proj5/Program.cs
@@ -10,6 +10,40 @@ namespace proj5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(IsPrime(49));
+            AllPrimes(49);
         }
+
+
+        static bool IsPrime(int num)
+        {
+
+      
+            for (int i = 2; i <=Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+                    
+            }
+            return true;
+        }
+
+        static void AllPrimes(int num)
+        {
+            for (int i = 2; i <= num; i++)
+            {
+
+                if (IsPrime(i))
+                    {
+
+                    Console.WriteLine(i + " ");
+                }
+            }
+
+        }
+
     }
 }
+
